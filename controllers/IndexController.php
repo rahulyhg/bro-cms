1fc356fa327f3959777cfdc3fdd323e3d450fe04
lucashogenboom <hogenboom456@gmail.com>
2019-05-28 12:01:04 +0200

    - rewrite the INdexController so it uses a view

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -1,24 +1,17 @@
 <?php
+
 class IndexController {
 
-    public $welcomeText = "Hello there!";   
+    $welcomeText = "Hello there!";   
 
-    public function __construct(){
-    }
+    public function __construct(){}
 
     public function index() {
-	echo '<!DOCTYPE html>
-			<html>
-			    <head>
-			        <title>Bros page</title>
-			    </head>
-			    <body>
-			        <h1>Bro page</h1>
-			        <p>Welcome to the bro page.</p>';
-			   
-        echo '<br/>';
-	echo "<label>$this->welcomeText</label>";
-	echo '<form><input type="text" name="textInputField"/></form>'; 
-	echo '</body></html>';
- }
+		$this->getView('index', ['welcomeText' => $this->welcomeText]);
+ 	}
+
+ 	public function getView($viewName, $params = []){
+ 		extract($params);
+ 		require_once(dirname(__FILE__).'/../views.'.$viewName.'.php');
+ 	}
 }
