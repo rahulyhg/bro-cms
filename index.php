46cd6cfce8d41ce78fa16c1b823eb43a2766a94c
lucashogenboom <hogenboom456@gmail.com>
2019-05-28 12:01:56 +0200

    - rewriting the index (overcomplacting things with a class)

--- index.php
+++ index.php
@@ -2,16 +2,5 @@
 
 require_once(dirname(__FILE__).'/controllers/IndexController.php');
 
-class Index {
-    public function show(){
-        echo 'Hello World';
-        $indexController = new IndexController();
-        $indexController->index();
-    }
-}
-
-$index = new Index();
-$index->show();
-
-echo 'test';
-echo 'test';
+$indexController = new IndexController();
+$indexController->index();
