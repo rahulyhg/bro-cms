9aad2f3f7b7e105c59876db6d5d0937acb1455cf
lucashogenboom <hogenboom456@gmail.com>
2019-05-28 12:02:15 +0200

    - added a view

---
+++ views/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Bros page</title>
+    </head>
+    <body>
+        <h1>Bro page</h1>
+        <p>Welcome to the bro page.</p>'
+        <label>$this->welcomeText</label>";
+		<form>
+			<input type="text" name="textInputField"/>
+		</form>'; 
+	</body>
+</html>
