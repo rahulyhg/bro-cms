f601ba7d94367e5aa5dd027207c1c18b4b23400c
lucashogenboom <hogenboom456@gmail.com>
2019-05-29 09:21:04 +0200

    - rewritten the from inputs names so its clear for wich model the values are - added javascript script tag - added js/main.js

--- views/index.php
+++ views/index.php
@@ -2,14 +2,15 @@
 <html>
     <head>
         <title>Bros page</title>
+        <script src="/js/main.js" type="text/javascript"></script>
     </head>
     <body>
         <h1>Bro page</h1>
         <p>Welcome to the bro page.</p>
         <label><?= $welcomeText ?></label>
 		<form action="/postHandler.php" method="post">
-			<input type="text" name="name_of_site"/>
-			<input type="text" name="url_of_site"/>
+			<input type="text" name="BrosWebsite[name]"/>
+			<input type="text" name="BrosWebsite[url]"/>
 			<input type="submit" value="submit"/>
 		</form>
 	</body>
