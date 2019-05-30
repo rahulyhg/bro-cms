987da71cb0adea86c48016d0a8a36a7b0209af32
lucashogenboom <hogenboom456@gmail.com>
2019-05-30 22:43:25 +0200

    - added the main header.php content.php and footer.php views

---
+++ views/content.php
@@ -0,0 +1,4 @@
+		<div class="content_container">
+			<h1><?= $title ?></h1>
+			<p class="content"><?= $content ?></p>
+		</div>
---
+++ views/footer.php
@@ -0,0 +1,7 @@
+		<footer>
+			<ul>
+				<li>test</li>
+			</ul>
+		</footer>
+	</body>
+</html>
